f8ed7ca34dbee3236f5a7f9aa42318ee0b771875
Lehirti <lehirti@inbox.com>
2012-04-14 14:10:26 +0200

    text image caching

--- Mindcraft/src/org/lehirti/mindcraft/intro/Masturbate.java
+++ Mindcraft/src/org/lehirti/mindcraft/intro/Masturbate.java
@@ -1,11 +1,18 @@
 package org.lehirti.mindcraft.intro;
 
 import org.lehirti.engine.events.EventNode;
+import org.lehirti.engine.events.Event.NullState;
+import org.lehirti.engine.res.ResourceCache;
 import org.lehirti.engine.res.text.TextKey;
 
-public class Masturbate extends EventNode {
+public class Masturbate extends EventNode<NullState> {
   public static enum Text implements TextKey {
     TEXT
+  }
+  
+  @Override
+  protected void loadImages() {
+    ResourceCache.get(Intro.MASTURBATE);
   }
   
   @Override
