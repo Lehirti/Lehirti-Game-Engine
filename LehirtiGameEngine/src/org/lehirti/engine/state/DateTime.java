5f16c4875390ffd3b2de8d5705fed7d88690ece5
Lehirti <lehirti@inbox.com>
2012-06-02 21:30:19 +0200

    implement DateTime.advanceBy()/advanceTo() methods

--- LehirtiGameEngine/src/org/lehirti/engine/state/DateTime.java
+++ LehirtiGameEngine/src/org/lehirti/engine/state/DateTime.java
@@ -56,17 +56,72 @@ public enum DateTime implements IntState {
   }
   
   public static void advanceBy(final int DDhhmmss) {
-    // TODO
+    final int seconds = DDhhmmss % 100;
+    final int DDhhmm = DDhhmmss / 100;
+    int minutes = DDhhmm % 100;
+    final int DDhh = DDhhmm / 100;
+    int hours = DDhh % 100;
+    int days = DDhh / 100;
+    
+    long newSeconds = StateObject.get(SECOND) + seconds;
+    while (newSeconds >= 60) {
+      newSeconds -= 60;
+      minutes++;
+    }
+    StateObject.set(SECOND, newSeconds);
+    
+    long newMinutes = StateObject.get(MINUTE) + minutes;
+    while (newMinutes >= 60) {
+      newMinutes -= 60;
+      hours++;
+    }
+    StateObject.set(MINUTE, newMinutes);
+    
+    long newHours = StateObject.get(HOUR) + hours;
+    while (newHours >= 24) {
+      newHours -= 24;
+      days++;
+    }
+    StateObject.set(HOUR, newHours);
+    
+    StateObject.set(DAY, StateObject.get(DAY) + days);
+    
     updateScreen();
   }
   
+  /**
+   * advance time to next hhmmss, possible advancing the day; will do nothing if current hhmmss equals parameter hhmmss
+   * 
+   * @param hhmmss
+   */
   public static void advanceTo(final int hhmmss) {
-    // TODO
+    final long currenthhmmss = (StateObject.get(HOUR) * 10000) + (StateObject.get(MINUTE) * 100)
+        + StateObject.get(SECOND);
+    if (hhmmss < currenthhmmss) {
+      StateObject.set(DAY, StateObject.get(DAY) + 1);
+    }
+    
+    final int seconds = hhmmss % 100;
+    final int hhmm = hhmmss / 100;
+    final int minutes = hhmm % 100;
+    final int hours = hhmm / 100;
+    StateObject.set(HOUR, hours);
+    StateObject.set(MINUTE, minutes);
+    StateObject.set(SECOND, seconds);
     updateScreen();
   }
   
+  /**
+   * advance time to next week day toDay and time hhmmss; will do nothing if current datetime equals parameter datetime
+   * 
+   * @param toDay
+   * @param hhmmss
+   */
   public static void advanceTo(final DayOfWeek toDay, final int hhmmss) {
-    // TODO
+    advanceTo(hhmmss);
+    while (toDay != getCurrentDayOfWeek()) {
+      StateObject.set(DAY, StateObject.get(DAY) + 1);
+    }
     updateScreen();
   }
   
@@ -77,9 +132,13 @@ public enum DateTime implements IntState {
     Main.STATS_AREA.addText(ResourceCache.get(getCurrentDayOfWeek()));
     Main.STATS_AREA.addText(ResourceCache.get(CommonText.BLANK));
     final TextWrapper time = ResourceCache.get(CommonText.TIME_FORMAT);
-    time.addParameter(String.valueOf(StateObject.get(HOUR)));
-    time.addParameter(String.valueOf(StateObject.get(MINUTE)));
-    time.addParameter(String.valueOf(StateObject.get(SECOND)));
+    time.addParameter(padTo2Digits(StateObject.get(HOUR)));
+    time.addParameter(padTo2Digits(StateObject.get(MINUTE)));
+    time.addParameter(padTo2Digits(StateObject.get(SECOND)));
     Main.STATS_AREA.addText(time);
+  }
+  
+  private static String padTo2Digits(final long l) {
+    return l < 10 ? "0" + l : String.valueOf(l);
   }
 }
