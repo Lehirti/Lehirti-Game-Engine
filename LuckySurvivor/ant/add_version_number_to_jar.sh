675e5f73519e1a1ae16bcec2c690b1c14908e918
Lehirti <lehirti@inbox.com>
2012-06-02 17:58:05 +0200

    do not create "jar-renamed-to-zip" during ant-build anymore

--- LuckySurvivor/ant/add_version_number_to_jar.sh
+++ LuckySurvivor/ant/add_version_number_to_jar.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 build=$(grep build= version)
 mv -v ../../../luckysurvivor/LuckySurvivor.jar ../../../luckysurvivor/LuckySurvivor-${build:6}.jar
-cp -v ../../../luckysurvivor/LuckySurvivor-${build:6}.jar ../../../luckysurvivor/LuckySurvivor-${build:6}.zip
