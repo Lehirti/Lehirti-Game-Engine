b71e9864c64281b5688a912b235523af44224f7a
Lehirti <lehirti@inbox.com>
2012-09-17 23:41:23 +0200

    upgraded text system to allow for TextKeys and State within texts
    e.g. is the player's name is "Generic Player 9001"
    
    Hello. My name is {org.lehirti.luckysurvivor.pc.PC$String.NAME}.
    
    would resolve to
    
    Hello. My name is Generic Player 9001.
    
    TODO: allow for shortcuts. e.g. {#Player.NAME}

--- LehirtiGameEngine/src/org/lehirti/engine/res/text/TextWrapper.java
+++ LehirtiGameEngine/src/org/lehirti/engine/res/text/TextWrapper.java
@@ -8,12 +8,22 @@ import java.io.ObjectOutput;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.lehirti.engine.res.ResourceCache;
 import org.lehirti.engine.res.ResourceState;
+import org.lehirti.engine.state.BoolState;
+import org.lehirti.engine.state.IntState;
+import org.lehirti.engine.state.ObjState;
+import org.lehirti.engine.state.State;
+import org.lehirti.engine.state.StringState;
 import org.lehirti.engine.util.FileUtils;
 import org.lehirti.engine.util.PathFinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TextWrapper implements Externalizable {
   private static final long serialVersionUID = 1L;
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(TextWrapper.class);
   
   private TextKey key;
   private String rawValue; // as stored on disc; may be different from what's displayed on screen
@@ -100,21 +110,63 @@ public class TextWrapper implements Externalizable {
   }
   
   public String getValue() {
-    if (this.parameters.isEmpty()) {
-      return getRawValue();
-    }
     String val = this.rawValue;
-    for (int i = 0; i < this.parameters.size(); i++) {
-      final Object param = this.parameters.get(i);
-      String paramString;
-      if (param instanceof String) {
-        paramString = (String) param;
-      } else {
-        paramString = ((TextWrapper) param).getValue();
-      }
-      val = val.replaceAll("\\{" + i + "\\}", paramString);
+    int fromIndex = 0;
+    while ((fromIndex = val.indexOf("{", fromIndex)) != -1) {
+      final int toIndex = val.indexOf("}", fromIndex + 1);
+      final String parameter = val.substring(fromIndex + 1, toIndex);
+      final String replacement = getParameterReplacement(parameter);
+      LOGGER.debug("Parameter \"{}\" found in \"{}\"", parameter, val);
+      val = val.replace("{" + parameter + "}", replacement);
     }
     return val;
+  }
+  
+  private String getParameterReplacement(final String parameter) {
+    // try explicitly set parameters
+    try {
+      final int parsedInt = Integer.parseInt(parameter);
+      if (parsedInt >= 0 && parsedInt < this.parameters.size()) {
+        final Object param = this.parameters.get(parsedInt);
+        if (param instanceof String) {
+          return (String) param;
+        } else {
+          return ((TextWrapper) param).getValue();
+        }
+      } else {
+        LOGGER.error("{} is missing explicitly set parameter #{}", toString(), parsedInt);
+        return "[MISSING PARAMETER #" + parameter + "]";
+      }
+    } catch (final NumberFormatException ignore) {
+      // is not an explicitly set parameter; go on trying other possibilities
+    }
+    
+    // try expanded FQCN
+    final int endOfClassname = parameter.lastIndexOf(".");
+    if (endOfClassname != -1) {
+      final String className = parameter.substring(0, endOfClassname);
+      final String name = parameter.substring(endOfClassname + 1);
+      try {
+        final Enum parameterValue = Enum.valueOf((Class<? extends Enum>) Class.forName(className), name);
+        if (parameterValue instanceof TextKey) {
+          return ResourceCache.get((TextKey) parameterValue).getValue();
+        } else if (parameterValue instanceof StringState) {
+          return State.get((StringState) parameterValue);
+        } else if (parameterValue instanceof IntState) {
+          return String.valueOf(State.get((IntState) parameterValue));
+        } else if (parameterValue instanceof ObjState) {
+          return State.get((ObjState) parameterValue).toString();
+        } else if (parameterValue instanceof BoolState) {
+          return String.valueOf(State.is((BoolState) parameterValue));
+        }
+      } catch (final ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    
+    LOGGER.error("Unable to resolve parameter \"{}\" for {}", parameter, toString());
+    return "[CANNOT RESOLVE PARAMETER \"" + parameter + "\"]";
   }
   
   public void setValue(final String value, final String contentDir) {
@@ -160,7 +212,7 @@ public class TextWrapper implements Externalizable {
   
   @Override
   public String toString() {
-    return this.key.getClass().getSimpleName() + "." + this.key.name() + "[" + getValue() + "]";
+    return this.key.getClass().getSimpleName() + "." + this.key.name() + "[" + this.rawValue + "]";
   }
   
   @Override
